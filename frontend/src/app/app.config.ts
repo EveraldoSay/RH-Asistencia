b45f45b26537231ec09cf8f0d9634fb6e0b0a261
Wilmer-abc <wbatzc@miumg.edu.gt>
2025-11-09 14:20:40 -0600

    nueva configuracion responsiv

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,12 +5,14 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 import { KEYCLOAK_INITIALIZER } from './keycloak-init';
 import { kcBearerInterceptor } from './interceptors/kc-bearer.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
 
     //Registramos nuestro interceptor explícitamente
+    provideAnimations(),
     provideHttpClient(withInterceptors([kcBearerInterceptor])),
 
     importProvidersFrom(KeycloakAngularModule),
