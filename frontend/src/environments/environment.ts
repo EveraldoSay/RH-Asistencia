62a6f9e335dc7c6e13104c0b2678f28323d6ec40
Wilmer-abc <wbatzc@miumg.edu.gt>
2025-11-14 11:13:39 -0600

    cambios

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiBase: 'http://localhost:3000/api',
   keycloak: {
-    url: 'http://localhost:8080',
+    url: 'https://sso.hro.gob.gt',
     realm: 'Hospital-O',
     clientId: 'frontend-angular'
   }
